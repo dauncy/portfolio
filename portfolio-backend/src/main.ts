1e4963389be7181db11365a649f3075a145922f7
Daniel Wilder <dauncy@gmail.com>
2025-04-18 10:02:40 -0700

    maybe fix deployment?

--- portfolio-backend/src/main.ts
+++ portfolio-backend/src/main.ts
@@ -3,7 +3,9 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const port = process.env.PORT || 3000;
+  const address = process.env.ADDRESS || '0.0.0.0';
   app.enableCors();
-  await app.listen(3000);
+  await app.listen(port, address);
 }
 bootstrap();
